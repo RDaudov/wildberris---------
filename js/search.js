52d24447c4d405edc4061b21c6d6c22177ebf949
Ramzes <rdaudov007@gmail.com>
2022-02-24 15:58:33 +0300

    first commit

---
+++ js/search.js
@@ -0,0 +1,15 @@
+const search = function () {
+    const input = document.querySelector('.search-block > input')
+    const searchBtn = document.querySelector('.search-block > button')
+
+  /*   input.addEventListener('input', (event) => {
+        console.log(event.target)
+    }) */
+
+    searchBtn.addEventListener('click', () => {
+        let inputValue = input.value
+        console.log(inputValue)
+    })
+}
+
+search()
